edad739ed23f4598231b111836dbfd6ef15d1c61
SimoneDeGasperis <simone.de.gasperis94@gmail.com>
2024-01-07 16:24:51 +0100

    Restructure repo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tutorialtestpackage',  # Replace with your actual package name
+    name='tutorialtestpackage3',  # Replace with your actual package name
     url = 'https://github.com/enomis-dev/test-package', # Replace with your github project link
     version='0.1.0',
     packages=find_packages(),
