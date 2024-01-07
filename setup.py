b06171bdf3dc5684c0d48fd1cc34b75e26b1b0ef
SimoneDeGasperis <simone.de.gasperis94@gmail.com>
2024-01-07 12:36:30 +0100

    Update README

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tutorial-test-package',  # Replace with your actual package name
+    url = 'https://github.com/enomis-dev/test-package', # Replace with your github project link
     version='0.1',
     packages=find_packages(),
     install_requires=[
